ece9c117379a5b837936cada82079d09e761b128
Patricia Castellanos <patricia.castellanos@nasa.gov>
2024-02-06 16:18:16 -0500

    update sample_geosit code to work for viirs

--- src/NNR/viirs/sample_geosit.py
+++ src/NNR/viirs/sample_geosit.py
@@ -4,7 +4,7 @@
 """
 
 import os, sys
-from pyabc.giant import GIANT
+from pyabc.giant_viirs import GIANT
 import argparse
 
 
@@ -13,15 +13,15 @@ if __name__ == '__main__':
     #   --------------------------
     parser = argparse.ArgumentParser()
     parser.add_argument("giantFile",help="giant spreadhseet file to base sampling on")
-    parser.add_argument("syear",help="start year")
-    parser.add_argument("eyear",help="end year")
+    parser.add_argument("sdate",help="start date")
+    parser.add_argument("edate",help="end date")
 
     args = parser.parse_args()
     outDir = os.path.dirname(args.giantFile)
     fname = os.path.basename(args.giantFile)
-    npzFile = outDir + '/' + fname[:-3] + '_GEOSIT_{}_{}.npz'.format(args.syear,args.eyear)
+    npzFile = outDir + '/' + fname[:-3] + '_GEOSIT_{}_{}.npz'.format(args.sdate.replace('-',''),args.edate.replace('-',''))
 
-    g = GIANT(args.giantFile,tymemin='{}-01-01T00'.format(args.syear),tymemax='{}-01-01T00'.format(args.eyear))
+    g = GIANT(args.giantFile)
     g.sampleMERRA(slv_x='slv_tavg_1hr_glo_L576x361_slv',aer_x='aer_tavg_3hr_glo_L576x361_slv',npzFile=npzFile,Verbose=True)
 
 

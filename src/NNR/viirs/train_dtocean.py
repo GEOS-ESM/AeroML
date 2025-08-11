cc0992f9346aa94da8202818779ada1fe47ae614
Patricia Castellanos <patricia.castellanos@nasa.gov>
2025-08-11 13:25:21 -0400

    fix noaa-20 naming

--- src/NNR/viirs/train_dtocean.py
+++ src/NNR/viirs/train_dtocean.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
 
     if sat == 'SNPP':
         retrieval    = 'VS_DT_OCEAN'
-    if sat == 'NOAA20':
+    if sat in ['NOAA20','NOAA-20']:
         retrieval    = 'VN20_DT_OCEAN'
 
     expid        = '{}_{}'.format(retrieval,expid)
